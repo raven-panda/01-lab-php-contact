e29ea814fa0927b387976b5bbb5ae61022248c91
Ambroise Maire <ambroise.maire7@gmail.com>
2023-09-19 11:26:21 +0200

    chore: 'Your Account' Dropdown Menu
    <Functions>
    functions.php :
    	- Created two functions :
    		- getToken() => it gets the token stored in the client.
    		- getUser() => it gets the user's firstname, name and email in the database where the token matches.
    	These functions need to be secured more though.
    
    <Dashboard>
    dashboard.php :
    	- Added a dropdown menu that displays the User's informations (firstname, name, email) and the available actions (like disconnect).
    
    <Style>
    style.scss :
    	- Using Sass preprocessor for stylesheets.
    	- Styled the user dropdown menu.

--- app/src/_library/php/functions.php
+++ app/src/_library/php/functions.php
@@ -14,4 +14,37 @@
             return false;
         }
     }
+    function getToken() {
+        if (isset($_COOKIE['token']) && !empty($_COOKIE['token'])) {
+            return $_COOKIE['token'];
+        } else if (isset($_SESSION['token']) && !empty($_SESSION['token'])) {
+            return $_SESSION['token'];
+        } else {
+            return false;
+        }
+    }
+    function getUser($token) {
+        $mysqlHost = getenv('MYSQL_HOST');
+        $mysqlDb = getenv('MYSQL_DATABASE');
+        $mysqlUser = getenv('MYSQL_USER');
+        $mysqlPw = getenv('MYSQL_PASSWORD');
+
+        try {
+            $dsn = 'mysql:host='. $mysqlHost .';dbname='. $mysqlDb .';charset=utf8';
+            $mysqlConnection = new PDO($dsn, $mysqlUser, $mysqlPw);
+    
+            $sql = 'SELECT firstname, name, email FROM user WHERE token = :token';
+            $sth = $mysqlConnection->prepare($sql);
+            $sth->bindParam(':token', $token, PDO::PARAM_STR);
+    
+            $sth->execute();
+
+            $result = $sth->fetch(PDO::FETCH_ASSOC);
+
+            return $result;
+        } catch (Exception $err) {
+            return $err;
+        }
+
+    }
 ?>
